67acbd654bd7f5b1d7de8bba364efb6e768b81f3
CamFreshwater <ccfreshwater@gmail.com>
2018-05-27 16:26:36 -0700

    Generate full dataset

--- scripts/prepCovariates.R
+++ scripts/prepCovariates.R
@@ -85,4 +85,7 @@ envCovar <-  covarDat %>%
 
 recDat <- merge(recDat, catchCovar[], by = "yr")
 recDat <- merge(recDat, envCovar, by = "entryYr")
+recDat <- with(recDat, recDat[order(stk, yr),])
+
+write.csv(recDat, here("/data/fullData.csv"), row.names = FALSE)
 

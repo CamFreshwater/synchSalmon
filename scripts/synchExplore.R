f6e7dfd25ab8bb8c6170315d90983066cf8fe736
CamFreshwater <ccfreshwater@gmail.com>
2018-06-18 16:32:16 -0700

    Add SR model residuals

--- scripts/synchExplore.R
+++ scripts/synchExplore.R
@@ -9,23 +9,48 @@
 #			in residuals
 #*************************************************************************************
 
-# setwd("C:/github/synchSalmon/")
-setwd("/Users/cam/github/synchSalmon") #Cam's Mac wd
+setwd("C:/github/synchSalmon/")
+# setwd("/Users/cam/github/synchSalmon") #Cam's Mac wd
 
 require(here); require(synchrony); require(zoo); require(ggplot2); require(dplyr); require(tidyr)
 
 source(here("scripts/synchFunctions.R"))
 
-## Convert long dataframe to wide matrix
-recDat <- read.csv(here("data/sox/fraserRecDatTrim.csv"), stringsAsFactors=F)
+## Data clean
+recDat1 <- read.csv(here("/data/sox/fraserRecDatEFF.csv"), stringsAsFactors = FALSE)
+recDat2 <- read.csv(here("/data/sox/fraserRecDatTrim.csv"), stringsAsFactors = FALSE) 
+recDat <- merge(recDat1, recDat2[, c("stk", "yr", "ets")], by = c("stk", "yr")) #combine ets and eff estimates
 recDat <- with(recDat, recDat[order(stk, yr),])
-ts <- recDat %>% group_by(stk) %>% summarise(tsLength=length(!is.na(ets)), firstYr=min(yr))
-selectedStks <- c(1, seq(from=3, to=11, by=1), 18, 19)
+recDat$prod <- log(recDat$rec/recDat$eff)
+for (i in 1:nrow(recDat)) { #add top-fitting SR model
+  stk <- recDat$stk[i]
+  if (stk == 1 | stk == 2 | stk == 6 | stk == 8 | stk == 9) {
+    recDat$model[i] <- "larkin"
+  }	else {
+    recDat$model[i] <- "ricker"
+  } 
+}
+# Add lagged eff abundances for larkin models
+recDat$eff1 <- NA
+recDat$eff2 <- NA
+recDat$eff3 <- NA
+stkIndex <- unique(recDat$stk)
+for(j in seq_along(stkIndex)) {
+  d <- subset(recDat, stk == stkIndex[j])
+  for (i in 1:nrow(d)) { #add top-fitting SR model
+    d$eff1[i] <- ifelse(i < 1, NA, d$eff[i - 1])
+    d$eff2[i] <- ifelse(i < 2, NA, d$eff[i - 2])
+    d$eff3[i] <- ifelse(i < 3, NA, d$eff[i - 3])
+  }
+  recDat[recDat$stk == stkIndex[j], c("eff1", "eff2", "eff3")] <- d[, c("eff1", "eff2", "eff3")]
+}
+# Trim and convert to matrix
+ts <- recDat %>% group_by(stk) %>% summarise(tsLength=length(!is.na(prod)), firstYr=min(yr))
+selectedStks <- c(1, seq(from=3, to=10, by=1), 18, 19)
 recDatTrim <- recDat[recDat$stk %in% selectedStks,]
-# recDat <- recDat[!recDat$stk == "15",] #remove stk with short TS
-# recDat <- recDat[!recDat$yr < "1973",] #remove earlier years to ensure time series balanced
+recDatTrim <- subset(recDatTrim, !is.na(prod) & !is.na(eff3))
 
-wideRec <- spread(recDatTrim[,c(1:3)], stk, ets)
+wideRec <- spread(recDatTrim[,c("stk", "yr", "ets")], stk, ets)
 recMat <- as.matrix(wideRec[,-1])
 
 
@@ -47,3 +72,24 @@ mtext(side=3, "Fraser Sockeye Metapopulation Dynamics", outer=TRUE)
 
 
 
+#_________________________________________________________________________
+## Fit Ricker and Larkin models to each and examine trends in residuals (accounts for 
+## density dependence and changes in exploitation rates through time)
+stkIndex <- unique(recDatTrim$stk)
+residVec <- NULL
+for(j in seq_along(stkIndex)) {
+  d <- subset(recDatTrim, stk == stkIndex[j])
+  mod <- unique(d$model)
+  if (mod == "ricker") {
+    srMod <- lm(prod ~ eff, data = d)
+  }
+  if (mod == "larkin") {
+    srMod <- lm(prod ~ eff + eff1 + eff2 + eff3, data = d)
+  }
+  residVec <- c(residVec, resid(srMod))
+}
+recDatTrim$modResid <- residVec
+
+
+recDat$mixedModResid <- mixedResid
+recDat$modResid <- residVec
--- scripts/synchSRModels.R
+++ scripts/synchSRModels.R
@@ -4,8 +4,8 @@
 # Inputs: stock-recruit and environmental data
 # Explainer: 1) Fits basic stock recruit models,
 #			 2) Incorporates environmental covariates and IDs top model
-# 			 3) Tests whether synchrony patters dissipate in residuals of top model 
-#               (i.e. is there evidence that synch is associated with environmental drivers)
+# 		 3) Tests whether synchrony patterns dissipate in residuals of top model 
+#         (i.e. is there evidence that synch is associated with environmental drivers)
 #*************************************************************************************
 
 # setwd("C:/github/synchSalmon/")

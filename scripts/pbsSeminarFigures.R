069c90be7fedc05d5e5d93216dbbe1c4133b3566
Cameron Freshwater <ccfreshwater@gmail.com>
2019-04-15 15:52:21 -0700

    update pres figures

--- scripts/pbsSeminarFigures.R
+++ scripts/pbsSeminarFigures.R
@@ -222,3 +222,133 @@ ggplot(synchDat, aes(x = yr, y = recDev, col = stkName)) +
   theme_sleekX() + 
   facet_wrap(~synch)
 dev.off()
+
+
+## Empty time series
+dum <-  outDat %>% 
+  filter(yr > 60,
+         cu == "4",
+         sigma == "low",
+         synch == "med")
+png(file = paste(here(),"/figs/presentationFigs/blankTS.png", sep = ""),
+    height = 1.5, width = 3, units = "in", res = 300)
+ggplot(dum, aes(x = yr, y = recDev)) +
+  geom_line(size = 1.5) +
+  labs(x = "Year", y = "Recruitment Deviation") +
+  theme_sleekX()
+dev.off()
+
+
+#------------
+
+
+### Synchrony simulation analysis
+# Plots of a) each PM at reference and b) juxtaposed w/ low
+
+## Load data
+plotDat <- read.csv(here("outputs/generatedData", "summaryTable_noSkew.csv"), 
+                    stringsAsFactors = F) %>% 
+  select(-X) %>% 
+  filter(!om == "Low Prod. Heavy Tails",
+         var %in% c("medRecRY", "ppnMixedOpen", "ppnYrsHighCatch", 
+                    "stabilityCatch")) %>% 
+  mutate(synch = as.factor(synch),
+         om = as.factor(om),
+         sigma = as.factor(sigma)) %>% 
+  mutate(synch = fct_relevel(synch, "high", after = 2),
+         sigma = fct_relevel(sigma, "high", after = 2),
+         om = fct_relevel(om, "Low Prod.", after = 1))
+
+colPal <- viridis(length(levels(plotDat$synch)), begin = 0, end = 1)
+names(colPal) <- levels(plotDat$synch)
+
+# Make individual plots
+pmPlot <- function(temp, catchYLab, facetCol = 3, pos = NULL, dotSize = 3.25, 
+                   lineSize = 0.8, legSize = 0.7, axSize = 10, 
+                   facetSize = 0.95) {
+  q <- ggplot(temp, aes(x = sigma, y = medn, ymin = lowQ, ymax = highQ,
+                   fill = synch)) +
+    labs(x = "Variance", y = catchYLab, 
+         fill = "Sim.\nParameter\nValue") +
+    geom_pointrange(shape = 21, fatten = dotSize, size = lineSize,
+                    position = position_dodge(width = 0.65)) +
+    scale_x_discrete(labels = c("low" = "Low",
+                                "med" = "Reference",
+                                "high" = "High")) +
+    scale_fill_manual(name = "Synchrony", values = colPal,
+                      labels = c("low" = "Low",
+                                 "med" = "Moderate",
+                                 "high" = "High")) +
+    theme_sleekX(legendSize = legSize, axisSize = axSize, facetSize = facetSize)
+  
+  if (length(unique(temp$om)) > 1) {
+    q <- q +
+      facet_wrap(~om, scales = "fixed", ncol = facetCol, nrow = 1)
+  }
+  if (is.null(pos) == FALSE) {
+    q <- q + theme_sleekX(position = pos, axisSize = axSize, 
+                          facetSize = facetSize)
+  }
+  return(q)
+}
+
+retDat <- plotDat %>% 
+  filter(var == "medRecRY")
+png(file = paste(here(),"/figs/presentationFigs/retPMRef.png", sep = ""),
+    height = 2.5, width = 5, units = "in", res = 300)
+pmPlot(retDat %>% filter(om == "Reference Prod."),
+       catchYLab = "Agg. Return\nAbundance", dotSize = 4.5)
+dev.off()
+
+png(file = paste(here(),"/figs/presentationFigs/retPM.png", sep = ""),
+    height = 2.5, width = 6, units = "in", res = 300)
+retPlot <- pmPlot(retDat, catchYLab = "Agg. Return\nAbundance", dotSize = 4.5,
+                  facetSize = 1.1)
+print(retPlot)
+dev.off()
+
+escDat <- plotDat %>% 
+  filter(var == "ppnMixedOpen")
+png(file = paste(here(),"/figs/presentationFigs/escPMRef.png", sep = ""),
+    height = 2.5, width = 5, units = "in", res = 300)
+pmPlot(escDat %>% filter(om == "Reference Prod."),
+       catchYLab = "Ppn. of Years\nEsc. Goal Met",
+       dotSize = 4.5)
+dev.off()
+
+png(file = paste(here(),"/figs/presentationFigs/escPM.png", sep = ""),
+    height = 2.5, width = 6, units = "in", res = 300)
+escPlot <- pmPlot(escDat, catchYLab = "Ppn. of Years\nEsc. Goal Met",
+                  dotSize = 4.5, facetSize = 1.1)
+print(escPlot)
+dev.off()
+
+stbDat <- plotDat %>% 
+  filter(var == "stabilityCatch")
+png(file = paste(here(),"/figs/presentationFigs/stbPMRef.png", sep = ""),
+    height = 2.5, width = 5, units = "in", res = 300)
+pmPlot(stbDat %>% filter(om == "Reference Prod."),
+       catchYLab = "Catch Stability",
+       dotSize = 4.5)
+dev.off()
+
+png(file = paste(here(),"/figs/presentationFigs/stbPM.png", sep = ""),
+    height = 2.5, width = 6, units = "in", res = 300)
+stbPlot <- pmPlot(stbDat, catchYLab = "Catch Stability", dotSize = 4.5, 
+                  facetSize = 1.1)
+print(stbPlot)
+dev.off()
+
+retPlot <- pmPlot(retDat, catchYLab = "Agg. Return\nAbundance", dotSize = 4.5,
+                  facetSize = 1.2, axSize = 13, pos = "top")
+escPlot <- pmPlot(escDat, catchYLab = "Ppn. of Years\nEsc. Goal Met",
+                  dotSize = 4.5, facetSize = 1.2, axSize = 13, pos = "mid")
+stbPlot <- pmPlot(stbDat, catchYLab = "Catch Stability", dotSize = 4.5, 
+                  facetSize = 1.2, axSize = 13, pos = "bottom")
+
+png(file = paste(here(),"/figs/presentationFigs/allPMs.png", sep = ""),
+    height = 7, width = 6, units = "in", res = 300)
+ggarrange(retPlot, escPlot, stbPlot,
+          ncol = 1, nrow = 3, common.legend = TRUE, legend = "right",
+          align = "v", heights = c(1.1,1,1.2))
+dev.off()
